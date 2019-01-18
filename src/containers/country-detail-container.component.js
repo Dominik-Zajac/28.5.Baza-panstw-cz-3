3e87aaf9ee4f28afb3118aeaef5e6be13cb9c037
Dominik Zając <zajac_dominik@onet.pl>
2019-01-18 17:22:37 +0100

    First commit

---
+++ src/containers/country-detail-container.component.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CountryDetail from '../presentational/country-details.component';
+import { getCountry } from '../actions/actions-countries';
+
+class CountryDetailsContainer extends Component {
+	componentDidMount() {
+		this.props.dispatch(getCountry(this.props.params.id));
+	}
+
+	render() {
+		return (
+			<CountryDetail country={ this.props.selectedCountry } />
+		)
+	} 
+}
+
+const mapStateToProps = function(store) {
+	return {
+		selectedCountry: store.countriesReducer.selectedCountry
+	};
+};
+
+export default connect(mapStateToProps)(CountryDetailsContainer);
---
+++ src/presentational/home.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import homeImage from '../Home.png';
+
+const Home = () => (
+	<div className='country-logo-wrapper'>
+		<h1> Witamy w aplikacji Panstwa</h1>
+		<img src={ homeImage } alt='Country' />
+	</div>
+);
+
+export default Home;
